d6a5a5c87f456c293673e555c4c57aa44f70bd3a
willgdm <william.gomes0405@gmail.com>
2024-11-26 15:26:39 -0300

    InventoryDatabase2

--- app/src/main/java/br/edu/up/rgm33026050/data/InventoryDatabase.kt
+++ app/src/main/java/br/edu/up/rgm33026050/data/InventoryDatabase.kt
@@ -1,4 +1,22 @@
 package br.edu.up.rgm33026050.data
 
-class InventoryDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import android.content.Context
+import androidx.room.Room
+
+@Database(entities = [Item::class], version = 1, exportSchema = false)
+abstract class InventoryDatabase : RoomDatabase() {
+    abstract fun itemDao(): ItemDao
+    companion object {
+        @Volatile
+        private var Instance: InventoryDatabase? = null
+        fun getDatabase(context: Context): InventoryDatabase {
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, InventoryDatabase::class.java, "item_database")
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+    }
 }
